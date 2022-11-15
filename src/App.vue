41962352f23e85e5a341e943d339e26f962b5c06
colopisalvatore <salvatorecolopi1989@gmail.com>
2022-11-15 16:34:52 +0100

    add ResultCounter

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
     <HeaderComponent />
     <main>
+        <ResultCount />
         <CharacterList />
     </main>
 </template>
@@ -9,10 +10,11 @@
 import axios from "axios";
 import HeaderComponent from './components/HeaderComponent.vue'
 import CharacterList from './components/CharacterList.vue';
+import ResultCount from './components/ResultCount.vue';
 
 import { store } from "./assets/data/store"
 export default {
-    components: { HeaderComponent, CharacterList, },
+    components: { HeaderComponent, CharacterList, ResultCount, },
     data() {
         return {
             store
@@ -22,7 +24,7 @@ export default {
         axios.get("https://www.breakingbadapi.com/api/characters").then((resp) => {
             this.store.loading = true;
             this.store.characters = resp.data;
-            console.log(this.store.characters);
+            // console.log(this.store.characters);
             this.store.loading = false;
         })
     }
---
+++ src/components/ResultCount.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="text-center">
+        <h4 class="text-center">Found {{ store.characters.length }} characters</h4>
+    </div>
+</template>
+
+<script>
+import { store } from "../assets/data/store"
+
+export default {
+    name: 'ResultCount',
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+h4 {
+    color:white;
+}
+
+</style>
